56b33a90e6349427e0a250d345a07f4ced09c17b
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2020-02-01 12:00:38 +0000

    Changed test to use scene.

--- test/io/github/tomaso2468/netengine/test/GameTest.java
+++ test/io/github/tomaso2468/netengine/test/GameTest.java
@@ -2,18 +2,18 @@ package io.github.tomaso2468.netengine.test;
 
 import java.io.IOException;
 
-import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
 import io.github.tomaso2468.netengine.EngineException;
 import io.github.tomaso2468.netengine.Game;
-import io.github.tomaso2468.netengine.camera.SimpleCamera3D;
 import io.github.tomaso2468.netengine.log.Log;
-import io.github.tomaso2468.netengine.render.MultiTextureVertexObject;
-import io.github.tomaso2468.netengine.render.RenderState;
 import io.github.tomaso2468.netengine.render.Renderer;
 import io.github.tomaso2468.netengine.render.Shader;
 import io.github.tomaso2468.netengine.render.Texture;
+import io.github.tomaso2468.netengine.scene3d.BasicObject3D;
+import io.github.tomaso2468.netengine.scene3d.Material;
+import io.github.tomaso2468.netengine.scene3d.Scene3D;
+import io.github.tomaso2468.netengine.scene3d.TextureMaterial;
 
 public class GameTest extends Game {
 
@@ -50,8 +50,7 @@ public class GameTest extends Game {
 		super.initGame();
 	}
 	
-	private RenderState state;
-	private MultiTextureVertexObject object;
+	private Scene3D scene;
 	private Shader shader;
 	private Texture texture;
 	private Texture texture2;
@@ -74,89 +73,25 @@ public class GameTest extends Game {
 			throw new EngineException(e);
 		}
 		
-		Log.debug("State");
-		state = renderer.createRenderState();
-		state.enterState();
+		scene = new Scene3D();
 		
-		object = renderer.createStaticVOMultiTexture(vertices, indices);
-		object.configureVO(0);
-		object.configureVOTexture(1);
-		object.configureVOSelect(2);
+		Material m = new TextureMaterial(shader, new Texture[] {texture, texture2});
 		
-		object.unbind();
-		state.leaveState();
+		BasicObject3D object = new BasicObject3D(renderer, vertices, indices, m);
+		object.setRotation(new Vector3f(0, 2, 0));
 		
-		camera = new SimpleCamera3D();
-		camera.setPosition(new Vector3f(0, 0, 3));
+		scene.add(object);
+		
+		scene.getCamera().setPosition(new Vector3f(-1, 0, 0));
 		
 		renderer.setDepthTest(true);
 		renderer.setCaptureMouse(true);
 	}
 	
-//	private Vector3f cameraPos = new Vector3f(0, 0, 3);
-//	private Vector3f cameraFront = new Vector3f(0, 0, -1);
-//	private Vector3f cameraUp = new Vector3f(0, 1, 0);
-//	private float yaw = (float) Math.toRadians(-80);
-//	private float pitch = 0;
-	
-	private SimpleCamera3D camera;
-	
 	@Override
 	protected void render(Renderer renderer) {
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_LEFT)) {
-//			yaw -= 0.05f;
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_RIGHT)) {
-//			yaw += 0.05f;
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_UP)) {
-//			pitch += 0.05f;
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_DOWN)) {
-//			pitch -= 0.05f;
-//		}
-//		cameraFront = new Vector3f(
-//				(float) (Math.cos(yaw) * Math.cos(pitch)),
-//				(float) (Math.sin(pitch)),
-//				(float) (Math.sin(yaw) * Math.cos(pitch)));
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_W)) {
-//			cameraPos = cameraPos.add(new Vector3f(cameraFront).mul(0.05f));
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_S)) {
-//			cameraPos = cameraPos.add(new Vector3f(cameraFront).mul(-0.05f));
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_A)) {
-//			cameraPos = cameraPos.add(new Vector3f(cameraFront).cross(cameraUp).mul(-0.05f));
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_D)) {
-//			cameraPos = cameraPos.add(new Vector3f(cameraFront).cross(cameraUp).mul(0.05f));
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_SPACE)) {
-//			cameraPos = cameraPos.add(new Vector3f(0, 0.05f, 0));
-//		}
-//		if (renderer.getInput().isKeyDown(GLFW_KEY_LEFT_SHIFT)) {
-//			cameraPos = cameraPos.add(new Vector3f(0, -0.05f, 0));
-//		}
+		scene.update(this, renderer.getInput(), 1 / 60f);
 		
-		camera.update(renderer.getInput(), 1f / 60);
-		
-		texture.bind(0);
-		texture2.bind(1);
-		
-		shader.startUse();
-		state.enterState();
-		
-		shader.setUniformMatrix4("view", camera.getView(renderer));
-		shader.setUniformMatrix4("projection", camera.getProjection(renderer));
-//		shader.setUniformMatrix4("view", new Matrix4f().lookAt(cameraPos, new Vector3f(cameraPos).add(cameraFront), cameraUp));
-//		shader.setUniformMatrix4("projection", new Matrix4f().setPerspective((float) Math.toRadians(45), 800 / 600, 0.1f, 100));
-		
-		shader.setUniformMatrix4("model", new Matrix4f().rotate(-1, new Vector3f(1, 0, 0)));
-		object.draw(renderer);
-		
-//		shader.setUniformMatrix4("model", new Matrix4f().translate(0, 0, -1).rotate(-1, new Vector3f(1, 0, 0)));
-//		object.draw(renderer);
-		
-		state.leaveState();
+		scene.draw(this, renderer);
 	}
 }
